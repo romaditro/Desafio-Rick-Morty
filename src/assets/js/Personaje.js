d88d39aec00863b3609f47dcae30cdcf79e70ca8
romaditro <sr.rdiazt@gmail.com>
2021-03-30 19:59:27 -0300

    Crenado las clases Serie-Personaje

---
+++ src/assets/js/Personaje.js
@@ -0,0 +1,7 @@
+class Personaje{
+    constructor (nombre){
+        this.nombre = nombre;
+    }
+}
+
+export default Personaje;
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,10 @@
 import "../../assets/scss/style.scss";
 import Serie from "./Serie"
-
+import Personaje from "./Personaje"
 
 const objeto = new Serie(1);
 console.log(objeto);
+
+
+const objetoper = new Personaje("Rick");
+console.log(objetoper);
